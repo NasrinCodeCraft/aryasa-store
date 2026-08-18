f1a60ca60de1abd4e528cd9da07a66d04d7cbda0
Nasrin <n.mokhtari@>
2026-08-18 16:15:10 +0330

    Add global Themes

--- apps/storefront/src/app/layout.tsx
+++ apps/storefront/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
+import "@fontsource-variable/vazirmatn"
 import "styles/globals.css"
 
 export const metadata: Metadata = {
@@ -8,10 +9,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
-    <html lang="en" data-mode="light">
-      <body>
-        <main className="relative">{props.children}</main>
-      </body>
+    <html lang="fa" dir="rtl" data-mode="light">
+    <body>
+    <main className="relative">{props.children}</main>
+    </body>
     </html>
   )
 }
